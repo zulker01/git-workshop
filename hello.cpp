f25de2b0617929e539e1819b64b6c1a40965fcae
Zulker <zulker02@gmail.com>
2018-03-29 16:15:19 +0600

    modify hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main ()
 {
+printf("Hello World.\n");
+printf("Hello World.\n");
 printf("Hello World\n");
 printf("Hello Git workshop\n");
+printf("hello.\n");
 return 0;
 }
